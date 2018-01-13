c18b0a4c4330cff29fdf26a040ec3ffd92c7ca5e
Kashyap Mukkamala <kmukkamala@egen.solutions>
2018-01-12 19:47:08 -0600

    updated for minikube

--- push.sh
+++ push.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-NAME="minikube"
+NAME="test-mini"
 
 IMAGE_VERSION="$(git rev-parse --short HEAD)"
 
+# Build Image
+
 docker build -t $NAME:$IMAGE_VERSION .
 
-echo "Built image successfully"
+# Create Deployment
+
+kubectl run $NAME --image=$NAME:$IMAGE_VERSION --port=8080
+
+# Create Service
+
+kubectl expose deployment $NAME --type=LoadBalancer
